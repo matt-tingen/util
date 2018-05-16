d94568b46bea04007bf92070a8f4fb5c53a8a356
Matt Tingen <matt.tingen@willowtreeapps.com>
2018-05-16 13:51:43 -0400

    Fix implicit any

--- src/mapToObject.test.ts
+++ src/mapToObject.test.ts
@@ -1,23 +1,35 @@
-import test, { Macro, TestContext } from 'ava'
-import mapToObject from './mapToObject'
+import test, { Macro, TestContext } from 'ava';
+import mapToObject from './mapToObject';
 
 const testMapToObject: Macro<TestContext> = <T, V>(
   t: TestContext,
   values: T[],
   iteree: (value: T) => [any, V],
-  expected: ReturnType<typeof mapToObject>,
+  expected: ReturnType<typeof mapToObject>
 ) => {
-  t.deepEqual(mapToObject(values, iteree), expected)
-}
-testMapToObject.title = providedTitle => `mapToObject handles ${providedTitle}`
+  t.deepEqual(mapToObject(values, iteree), expected);
+};
+testMapToObject.title = providedTitle => `mapToObject handles ${providedTitle}`;
 
-test('identity', testMapToObject, [1, 2, 3], value => [value, value], {
-  1: 1,
-  2: 2,
-  3: 3,
-})
-test('index', testMapToObject, [1, 2, 3], (value, i) => [i, value], {
-  0: 1,
-  1: 2,
-  2: 3,
-})
+test(
+  'identity',
+  testMapToObject,
+  [1, 2, 3],
+  (value: number) => [value, value],
+  {
+    1: 1,
+    2: 2,
+    3: 3,
+  }
+);
+test(
+  'index',
+  testMapToObject,
+  [1, 2, 3],
+  (value: number, i: number) => [i, value],
+  {
+    0: 1,
+    1: 2,
+    2: 3,
+  }
+);
