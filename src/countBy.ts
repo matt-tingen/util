a393f0e2966c0ac7103a293c9d6d5fabf0f236ac
Matt Tingen <matt.tingen@gmail.com>
2021-06-16 18:47:57 -0400

    Update TS

--- src/countBy.ts
+++ src/countBy.ts
@@ -3,7 +3,7 @@ import identity from './identity';
 function countBy<T>(items: T[], iteree: (value: T) => any = identity) {
   const counts: { [value: string]: number } = {};
 
-  items.forEach(item => {
+  items.forEach((item) => {
     const value = iteree(item);
 
     if (!counts[value]) {
