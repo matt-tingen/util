a1570a683f79b64a71c4387ccef580f6c3322f0a
Matt Tingen <matt.tingen@gmail.com>
2022-01-29 17:49:22 -0500

    Fix repeat internal type

--- src/repeat.ts
+++ src/repeat.ts
@@ -1,6 +1,6 @@
 function repeat<T>(value: (i: number) => T, repeats: number): T[];
 function repeat<T>(value: T, repeats: number): T[];
-function repeat<T>(value: T, repeats: number): T[] {
+function repeat<T>(value: (i: number) => T | T, repeats: number): T[] {
   const array: T[] = [];
 
   for (let i = 0; i < repeats; i++) {
